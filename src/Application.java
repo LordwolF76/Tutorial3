53ff98d6d50439566f9db1c4101cdf7b9bb8b154
LordwolF76 <lordwolf76@gmail.com>
2016-07-31 11:48:29 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,30 @@
+/**
+ * Created by LordwolF on 7/31/2016.
+ */
+public class Application {
+    public static void main(String[] args) {
+        int myInt = 7;
+
+        String example = "This is a String";
+
+        String text = "Hello";
+
+        String blank = "  ";
+
+        String name = "LordwolF76";
+
+        String greeting = text + blank + name;
+
+        System.out.println(myInt);
+        System.out.println(text);
+        System.out.println(blank);
+        System.out.println(name);
+
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "LordwolF");
+
+        System.out.println("My integer is " + myInt);
+
+    }
+}
